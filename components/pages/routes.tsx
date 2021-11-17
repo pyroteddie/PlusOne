855311e952c1dae7241fd528f104361a2da55299
pyroteddie <liammatulick@gmail.com>
2021-11-17 17:59:03 +1000

    hf

--- components/pages/routes.tsx
+++ components/pages/routes.tsx
@@ -2,7 +2,7 @@ import React, { lazy, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './homePage';
 import SignIn from './signIn';
-
+import NotFound from './404'
 
 export default () => {
                                     /* webpackChunkName: "modulePublic"  */
@@ -10,8 +10,9 @@ export default () => {
 
   return <Router>
             <Routes>
-                
+                <Route exact path="*" element={ <NotFound /> } />
                 <Route exact path="/home" element= { <HomePage /> } />
+                
                 <Route exact path="/" element={ <SignIn /> } />
             </Routes>
         </Router>
