67629607e8738cddbee96f41d13d967aa3104e4b
liam matulick <liammatulick@gmail.com>
2021-11-20 17:34:24 +1000

    fix error in files

--- components/pages/routes.tsx
+++ components/pages/routes.tsx
@@ -1,4 +1,5 @@
-import React, { lazy, useEffect } from 'react';
+
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './homePage';
 import SignIn from './signIn';
