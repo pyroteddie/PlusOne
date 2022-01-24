4b3117b65d11dc0a92ace5fb09bdae96b77ba277
Liam matulick <liammatulick@gmail.com>
2022-01-25 01:15:25 +1000

    Update index.tsx

--- screens/signin/index.tsx
+++ screens/signin/index.tsx
@@ -8,15 +8,15 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 type signInScreenProp = StackNavigationProp<RootStackParamsList, 'SignIn'>;
 
- function signInScreen() {
+ function SignInScreen() {
     // put all varis and information here
   const navigation = useNavigation<signInScreenProp>();
 return( 
   <LinearGradient  colors={['#03E1EF', '#834AFC','#444EA6']} style={style.background}>
-      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-                <Text>PlusOne</Text>
+      <View style={{flex:0.5, alignItems:'center', justifyContent:'center'}}>
+                <View style={{flex:1, flexDirection: "row",}}><View ><Text style={{fontSize:40,color:'#6877FF', fontFamily:'Sitka Text'}}>Plus</Text></View><View ><Text style={{fontSize:40,color:'white',fontFamily:'Sitka Text', fontWeight:700,}}>One</Text></View></View>
                 <TextInput placeholder='Email' style={style.signInForm} />
-                <TextInput placeholder='Password'  style={style.signInForm} />
+                <TextInput secureTextEntry={true} placeholder='Password'  style={style.signInForm} />
                 <Pressable onPress={()=> navigation.navigate('Main')} style={style.ButtonSign}><Text style={style.buttonText}>Sign In</Text> </ Pressable>
       </View>
       </LinearGradient>
@@ -31,6 +31,8 @@ const style = StyleSheet.create({
     width:300,
     backgroundColor:"white",
     marginBottom:5,
+    padding:10,
+    fontSize:16,
 
   },
   ButtonSign:{
@@ -48,6 +50,8 @@ const style = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     lineHeight: 40,
+    fontSize:20,
+    fontWeight:700,
     
   },
 
@@ -55,4 +59,4 @@ const style = StyleSheet.create({
     flex:1,
   }
 });
-export default signInScreen;
+export default SignInScreen;
